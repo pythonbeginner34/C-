9ed5d8a6ec9c6e911fc05296f7709774c13309ef
wangjong <2731473097@qq.com>
2021-05-23 22:49:52 +0800

    数组

--- 数组/数组/二维数组.c
+++ 数组/数组/二维数组.c
@@ -73,3 +73,79 @@
 //printf("%p\n", arr + 1);//相差40
 //printf("%p\n", &arr);
 //printf("%p\n", &arr+1);//相差4
+
+//int main()
+//{
+//	char a = (int)'1';
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
+
+
+//void init(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = 0;
+//	}
+//}
+//
+//void print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		pirntf("%d\n", arr[i]);
+//	}
+//}
+//
+//void reverse(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//
+//	while (left < right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	init(arr, sz);
+//	print(arr, sz);
+//	reverse(arr, sz);
+//	print(arr, sz);
+//	return 0;
+//}
+//
+
+//交换数组元素
+int main()
+{
+	int arr1[] = { 1,3,5,7,9 };
+	int arr2[] = { 2,4,6,8,10 };
+
+	int arr[5] = { 0 };
+
+	int sz = sizeof(arr1) / sizeof(arr[0]);
+	int i = 0; 
+
+	for (i = 0; i < sz; i++)
+	{
+		int tmp = arr1[i];
+		arr1[i] = arr2[i];
+		arr2[i] = tmp;
+	}
+	return 0;
+}
