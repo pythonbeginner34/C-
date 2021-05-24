f5927e2d6f14ee821667119dffe3d030f658150d
wangjong <2731473097@qq.com>
2021-05-24 23:56:44 +0800

    作业练习

--- 作业练习/作业练习/practice.c
+++ 作业练习/作业练习/practice.c
@@ -488,28 +488,52 @@
 //	return 0;
 //}
 
-#include<stdio.h>
-#include<assert.h>
+//#include<stdio.h>
+//#include<assert.h>
+//
+//char* my_strcpy( char* dest, const char* src)
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	char* start = dest;
+//	拷贝src指向的字符串到dest指向的空间， 包括'\0'
+//	while (*dest++ == *src++)
+//	{
+//		;
+//	}
+//	返回目的地的起始地址
+//	return start;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "abcdefghi";
+//	char arr2[] = "bit";
+//	my_strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
 
-char* my_strcpy( char* dest, const char* src)
-{
-	assert(dest != NULL);
-	assert(src != NULL);
-	char* start = dest;
-	//拷贝src指向的字符串到dest指向的空间， 包括'\0'
-	while (*dest++ == *src++)
-	{
-		;
-	}
-	//返回目的地的起始地址
-	return start;
-}
 
-int main()
-{
-	char arr1[] = "abcdefghi";
-	char arr2[] = "bit";
-	my_strcpy(arr1, arr2);
-	printf("%s\n", arr1);
-	return 0;
-}
+//#include<stdio.h>
+//#define N 5
+//
+//int main()
+//{
+//	int i = 0;
+//	int k = 0;
+//	printf("请输入要查找的数: \n");
+//	scanf("%d", &k);
+//	int arr[N][N] = { {1,2,3,4,5}, {2,3,4,5,6}, {3,4,5,6,7}, {4,5,6,7,8},{5,6,7,8,9} };
+//	int(*p)[5] = arr + N - 1;
+//	int* q = *p + N - 1;
+//	for (i = N; i >= 0; i--)
+//	{
+//		if (k < q)
+//		{
+//
+//		}
+//	}
+//
+//}
+
