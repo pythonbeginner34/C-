647a35c562cf5df2d8d81de78f3103335c359e3e
wangjong <2731473097@qq.com>
2021-07-26 10:54:11 +0800

    递归的复习练习

---
+++ 复习递归.c/复习递归.c/递归.c
@@ -0,0 +1,243 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//void print(unsigned int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%u", &num);
+//	print(num);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<string.h>
+//模拟实现strlen函数
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+//递归模拟实现strlen函数
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//	{
+//		return 1 + my_strlen(str + 1);
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//	printf("%d\n", my_strlen(arr));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	
+//	int n = 0;
+//	int i = 0;
+//	int ret = 1;
+//	printf("请输入一个数\n");
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int Fac(int n)
+//{
+//	if (n <= 1)
+//	{
+//		return 1;
+//	}
+//	else 
+//	{
+//		return n * Fac(n - 1);
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	printf("请输入一个数\n");
+//	scanf("%d", &n);
+//	printf("%d\n", Fac(n));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int Fib(int n)
+//{
+//	if (n <= 2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return Fib(n - 1) + Fib(n - 2);
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", Fib(n));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1; 
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", Fib(n));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0, b = 0;
+//	for (a = 1, b = 1; a <= 100; a++)
+//	{
+//		if (b >= 20)
+//		{
+//			break;
+//		}
+//		if (b % 3 == 1)
+//		{
+//			b = b + 3;
+//			continue;
+//		}
+//		b = b - 5;
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 10 == 9)
+//		{
+//			count++;
+//		}
+//		if (i / 10 == 9)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 2 == 0)
+//		{
+//			sum -= 1.0 / i;
+//		}
+//		else
+//		{
+//			sum += 1.0 / i;
+//		}
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += flag * 1.0 / i;
+//		flag = -flag;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 };
+//	int max = arr[0];
+//	int i = 0;
+//	for (i = 1; i < 10; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("%d\n", max);
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 1; i <= 9; i++)
+	{
+		int j = 0;
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%-2d ", i, j, i * j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
